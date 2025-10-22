ed1b38f2d77f196091eb6be1a1f42bea0cd28bed
dinoslavik <dino.slavik@gmail.com>
2025-10-22 17:52:58 +0300

    Added remembering structures to checker
    This important to have ability to check what custom types could be used in the code

--- ignis/checker.py
+++ ignis/checker.py
@@ -1,4 +1,47 @@
 from ast_nodes import *
+from lexer import TokenType, Token
+
+
+class SymbolTable:
+    def __init__(self):
+        # Стек областей видимості. Кожен елемент - це словник {ім'я_символу: інформація}.
+        # Починаємо з глобальної області видимості.
+        self.scopes = [{}]
+
+    def enter_scope(self):
+        """Входимо в нову область видимості (наприклад, тіло функції або блок)."""
+        self.scopes.append({})
+
+    def exit_scope(self):
+        """Виходимо з поточної області видимості."""
+        assert len(self.scopes) > 1, "Cannot exit the global scope."
+        self.scopes.pop()
+
+    # ### MODIFIED ###: Замість фіксованих параметрів (is_mutable),
+    # ми приймаємо словник 'properties'. Це робить систему гнучкою
+    # для майбутнього додавання властивостей, таких як 'bits', 'encoding' тощо.
+    def add_symbol(self, name, symbol_type, properties=None):
+        """
+        Додає новий символ до поточної (найглибшої) області видимості.
+        Повертає True, якщо символ успішно додано, і False, якщо він вже існує.
+        """
+        if properties is None:
+            properties = {}
+
+        if name in self.scopes[-1]:
+            return False  # Символ вже оголошено в цій області
+        self.scopes[-1][name] = {'type': symbol_type, 'properties': properties}
+        return True
+
+    def lookup_symbol(self, name):
+        """
+        Шукає символ, починаючи з поточної області видимості і рухаючись до глобальної.
+        Повертає інформацію про символ або None, якщо його не знайдено.
+        """
+        for scope in reversed(self.scopes):
+            if name in scope:
+                return scope[name]
+        return None
 
 class NodeVisitor:
     def visit(self, node):
@@ -16,6 +59,8 @@ class NodeVisitor:
 class Checker(NodeVisitor):
     def __init__(self, reporter):
         self.reporter = reporter
+        self.symbol_table = SymbolTable()
+        self.struct_info = {}
 
     def _get_token_from_node(self, node):
         if hasattr(node, 'token'): return node.token
@@ -41,14 +86,67 @@ class Checker(NodeVisitor):
             elif isinstance(value, AST) and self._has_break(value): return True
         return False
 
+    # Реалізує двопрохідний аналіз: спочатку збираємо всі структури,
+    # а потім перевіряємо решту коду.
+    def visit_Program(self, node):
+        for decl in node.declarations:
+            if isinstance(decl, StructDef):
+                self.visit(decl)
+
+        # Другий прохід: перевіряємо решту оголошень (функції, глобальні змінні)
+        for decl in node.declarations:
+            if not isinstance(decl, StructDef):
+                self.visit(decl)
+
+    def visit_StructDef(self, node: StructDef):
+        struct_name = node.name
+        if struct_name in self.struct_info:
+            self.reporter.error(
+                "SE001", f"Struct '{struct_name}' is already defined.",
+                self._get_token_from_node(node)
+            )
+            return
+
+        struct_type = Type(Token(TokenType.IDENTIFIER, struct_name))
+        self.symbol_table.add_symbol(struct_name, struct_type, properties={'is_type': True})
+
+        fields = {}
+        for field in node.fields:
+            field_name = field.var_node.value
+            if field_name in fields:
+                self.reporter.error(
+                    "SE002", f"Duplicate field '{field_name}' in struct '{struct_name}'.",
+                    self._get_token_from_node(field.var_node)
+                )
+            else:
+                fields[field_name] = field.type_node
+
+        self.struct_info[struct_name] = {'fields': fields}
+
+    def visit_Block(self, node):
+        self.symbol_table.enter_scope()
+        for child in node.children:
+            self.visit(child)
+        self.symbol_table.exit_scope()
+
     def visit_LoopStmt(self, node):
         if not self._has_break(node.body):
-            self.reporter.warning("W001", "'loop' statement has no 'break' and may run forever.", self._get_token_from_node(node))
+            self.reporter.warning(
+                "W001", "'loop' statement has no 'break' and may run forever.",
+                self._get_token_from_node(node)
+            )
+        self.symbol_table.enter_scope()
         self.generic_visit(node)
+        self.symbol_table.exit_scope()
 
     def visit_WhileStmt(self, node):
         is_constant_true = isinstance(node.condition, Num) and node.condition.value != 0
         if is_constant_true:
             if not self._has_break(node.body):
-                self.reporter.warning("W002", "'while' loop with a constant true condition has no 'break' and may run forever.", self._get_token_from_node(node))
+                self.reporter.warning(
+                    "W002", "'while' loop with a constant true condition has no 'break' and may run forever.",
+                    self._get_token_from_node(node)
+                )
+        self.symbol_table.enter_scope()
         self.generic_visit(node)
+        self.symbol_table.exit_scope()
